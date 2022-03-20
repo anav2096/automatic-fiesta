ffe224627bb9f368c06e8669d69cb9c9f6f9a0d5
anav <anav2096@gmail.com>
2022-03-20 19:27:54 -0400

    file restructure

--- routes/api/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.use(require('./departmentRoutes'));
-router.use(require('./routeRoutes'));
-router.use(require('./employeeRoutes'));
-
-
-module.exports = router;
