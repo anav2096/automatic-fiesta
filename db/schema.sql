8555b28cb552603e87c198294e2a3134c61ab50b
anav <anav2096@gmail.com>
2022-03-19 20:14:24 -0400

    add routes, add seed, add schema

---
+++ db/schema.sql
@@ -0,0 +1,29 @@
+DROP TABLE IF EXISTS department;
+DROP TABLE IF EXISTS role;
+DROP TABLE IF EXISTS employee;
+
+
+CREATE TABLE department (
+  id INTEGER PRIMARY KEY,
+  name VARCHAR(50) NOT NULL,
+  description TEXT
+);
+
+CREATE TABLE role (
+  id INTEGER PRIMARY KEY,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL,
+  department_id INTEGER,
+  /*CONSTRAINT fk_party
+   FOREIGN KEY (party_id)
+    REFERENCES parties(id)
+    ON DELETE SET NULL*/
+);
+
+CREATE TABLE employee (
+  id INTEGER PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INTEGER,
+  manager_id INTEGER,
+);
